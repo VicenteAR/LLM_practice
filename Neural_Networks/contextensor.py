f3bf912c2fe3982b9918fe136a53e946178859f4
= <=>
2025-07-21 21:21:55 +0200

    Include new file with ContextTorchTensor class. Returns a set of torch tensors from a name list file.

---
+++ Neural_Networks/contextensor.py
@@ -0,0 +1,49 @@
+import random
+import torch
+
+
+class ContextTorchTensor:
+    """
+    Transform a name list into pytorch tensors.
+
+    This class takes a txt file containing names (or any string list) and
+    transforms it into a set X, Y pytorch tensors. Each character is
+    displayed as a integer. For example, 'a' -> 1, 'b' -> 2, etc.
+    The tensors are 'context'-dimensional, meaning that each row contains
+    n elements, n given by the selected context.
+    """
+
+    def __init__(self, context) -> None:
+        self.context = context
+        self.vocab_size = None
+
+    def _generate_abc(self):
+        self.abc = ["*"] + sorted(list(set("".join(self.words))))
+        self.stoi = {ch: ix for ix, ch in enumerate(self.abc)}
+        self.itos = {ix: ch for ch, ix in self.stoi.items()}
+        # Define the size of the dictionary. This is given by the number of characters.
+        self.vocab_size = len(self.stoi)
+
+    def open(self, file: str):
+        with open(file, "r") as f:
+            self.words = f.read().splitlines()
+        self._generate_abc()
+        # randomize the order of the words
+        random.seed(42)
+        random.shuffle(self.words)
+
+    def get_tensors(self, depth: int = -999) -> tuple[torch.Tensor, torch.Tensor]:
+        X = []
+        Y = []
+        # extension of the problem. How many words the module takes from the words attribute.
+        ext = len(self.words) if depth == -999 else depth
+        for word in self.words[:ext]:
+            context = [0] * self.context
+            for ch in list(word) + ["*"]:
+                ix = self.stoi[ch]
+                X.append(context)
+                Y.append(ix)
+                context = context[1:] + [ix]
+        X = torch.tensor(X)
+        Y = torch.tensor(Y)
+        return X, Y
